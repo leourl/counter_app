58afa92802d6f26c17cef19c00801590d340e682
Leonardo <contatos.leo@gmail.com>
2020-06-26 23:18:21 -0300

    teste

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         drawer: Drawer(
           child: Text(""),
@@ -20,15 +21,82 @@ class Home extends StatelessWidget {
           ),
           iconTheme: IconThemeData(color: Colors.black87),
           actions: [
-            IconButton(
-              icon: Icon(
-                Icons.add_circle,
+            Padding(
+              padding: const EdgeInsets.only(right: 8),
+              child: IconButton(
+                icon: Icon(
+                  Icons.add_circle,
+                  size: 38,
+                  color: Colors.blueAccent,
+                ),
+                onPressed: () {},
               ),
-              onPressed: () {},
             ),
           ],
         ),
-        body: Text("teste"),
+        body: ListView(
+          children: [
+            Card(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Nome_Contagem",
+                            ),
+                            Text("0026",
+                                style: TextStyle(
+                                    fontSize: 56, color: Colors.black87))
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 30, top: 5),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(right: 45),
+                              child: IconButton(
+                                icon: Icon(
+                                  Icons.remove,
+                                  size: 62,
+                                  color: Colors.blueAccent,
+                                ),
+                                onPressed: () {},
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: IconButton(
+                                icon: Icon(
+                                  Icons.add,
+                                  size: 62,
+                                  color: Colors.blueAccent,
+                                ),
+                                onPressed: () {},
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
